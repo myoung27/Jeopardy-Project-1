14157a6c40c6f9ac83100f73c88f98cccbe8ae68
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-11 19:58:57 -0400

    12. Correctly Added function to add points to single player score

--- knowit.js
+++ knowit.js
@@ -1,17 +1,36 @@
 let anwser10a = document.querySelector("#a10a");
 let anwser10aa = document.querySelector("#a10aa");
-let a10aaa = document.querySelector("#aa10aaa");
+let anwser10aaa = document.querySelector("#a10aaa");
+let anwser10b = document.querySelector("#a10b");
+let anwser10bb = document.querySelector("#a10bb");
+let anwser10bbb = document.querySelector("#a10bbb");
+let anwser10c = document.querySelector("#a10b");
+let anwser10cc = document.querySelector("#a10bb");
+let anwser10ccc = document.querySelector("#a10bbb");
 let score1 = document.querySelector("h1");
 let points = 0;
 anwser10a.addEventListener("click", function() {
-  let newScore = points + 10;
-  score1.innerHTML = newScore;
+    points -= 10
+    score1.innerHTML = points;
 });
-anwser10a.addEventListener("click", function() {
-    let newScore = points + 10;
-    score1.innerHTML = newScore;
+anwser10aa.addEventListener("click", function() {
+    points += 10
+    score1.innerHTML = points;
   });
-  anwser10a.addEventListener("click", function() {
-    let newScore = points + 10;
-    score1.innerHTML = newScore;
+  anwser10aaa.addEventListener("click", function() {
+    points -= 10
+    score1.innerHTML = points;
   });
+
+  anwser10b.addEventListener("click", function() {
+    points += 10
+    score1.innerHTML = points;
+  });
+  anwser10bb.addEventListener("click", function() {
+    points += 10
+    score1.innerHTML = points;
+    });
+    anwser10bbb.addEventListener("click", function() {``
+      let newScore = points - 10;
+      score1.innerHTML = newScore;
+    });
