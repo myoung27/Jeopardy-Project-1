bb55ad493fa66ba49364926fc415d89911fc5ed3
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-11 18:18:06 -0400

    11. Redid points to make it for 1 player

--- knowit.js
+++ knowit.js
@@ -1,154 +1,17 @@
-const points10a = 10;
-const points10b = 10;
-const points10c = 10;
-const points10d = 10;
-const points10e = 10;
-const points20a = 20;
-const points20b = 20;
-const points20c = 20;
-const points20d = 20;
-const points20e = 20;
-const points30a = 30;
-const points30b = 30;
-const points30c = 30;
-const points30d = 30;
-const points30e = 30;
-const points40a = 40;
-const points40b = 40;
-const points40c = 40;
-const points40d = 40;
-const points40e = 40;
-const points50a = 50;
-const points50b = 50;
-const points50c = 50;
-const points50d = 50;
-const points50e = 50;
-const question10a = 10;
-const question10b = 10;
-const question10c = 10;
-const question10d = 10;
-const question10e = 10;
-const question20a = 20;
-const question20b = 20;
-const question20c = 20;
-const question20d = 20;
-const question20e = 20;
-const question30a = 30;
-const question30b = 30;
-const question30c = 30;
-const question30d = 30;
-const question30e = 30;
-const question40a = 40;
-const question40b = 40;
-const question40c = 40;
-const question40d = 40;
-const question40e = 40;
-const question50a = 50;
-const question50b = 50;
-const question50c = 50;
-const question50d = 50;
-const question50e = 50;
-const anwser10a = 10;
-const anwser10aa = 10;
-const anwser10aaa = 10;
-const anwser10b = 10;
-const anwser10bb = 10
-const anwser10bbb = 10;
-const anwser10c = 10;
-const anwser10cc = 10
-const anwser10ccc = 10
-const anwser10d = 10;
-const anwser10dd = 10;
-const anwser10ddd = 10;
-const anwser10e = 10
-const anwser10ee = 10;
-const anwser10eee = 10;
-const anwser20a = 10;
-const anwser20aa = 10;
-const anwser20aaa = 10;
-const anwser20b = 10;
-const anwser20bb = 10
-const anwser20bbb = 10;
-const anwser20c = 10;
-const anwser20cc = 10
-const anwser20ccc = 10
-const anwser20d = 10;
-const anwser20dd = 10;
-const anwser20ddd = 10;
-const anwser20e = 10
-const anwser20ee = 10;
-const anwser20eee = 10;
-const anwser30a = 10;
-const anwser30aa = 10;
-const anwser30aaa = 10;
-const anwser30b = 10;
-const anwser30bb = 10
-const anwser30bbb = 10;
-const anwser30c = 10;
-const anwser30cc = 10
-const anwser30ccc = 10
-const anwser30d = 10;
-const anwser30dd = 10;
-const anwser30ddd = 10;
-const anwser30e = 10
-const anwser30ee = 10;
-const anwser30eee = 10;
-const anwser40a = 10;
-const anwser40aa = 10;
-const anwser40aaa = 10;
-const anwser40b = 10;
-const anwser40bb = 10
-const anwser40bbb = 10;
-const anwser40c = 10;
-const anwser40cc = 10
-const anwser40ccc = 10
-const anwser40d = 10;
-const anwser40dd = 10;
-const anwser40ddd = 10;
-const anwser40e = 10
-const anwser40ee = 10;
-const anwser40eee = 10;
-const anwser50a = 10;
-const anwser50aa = 10;
-const anwser50aaa = 10;
-const anwser50b = 10;
-const anwser50bb = 10
-const anwser50bbb = 10;
-const anwser50c = 10;
-const anwser50cc = 10
-const anwser50ccc = 10
-const anwser50d = 10;
-const anwser50dd = 10;
-const anwser50ddd = 10;
-const anwser50e = 10
-const anwser50ee = 10;
-const anwser50eee = 10;
-
-/* Get elements
-const modal = document.getElementById('simpleModal');
-//Get open modal button
-const modalBtn = document.getElementById('modalBtn');
-
-//Get Close button
-const closeBtn = document.getElementsByClassName('closeBtn')[0];
-//Listen for open click
-modalBtn.addEventListener('click', openModal);
-//Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick )
-
-// Function to open the modal
-
-function openModal() {
-    modal.style.display = 'block';
-}
-// Function to close modal
-function closeModal() {
-    modal.style.display = 'none';
-}
-//Function to close modal upon outside click
-function outsideClick(e){
-    if(e.target == modal){
-    modal.style.display = 'none';
-}}
+let anwser10a = document.querySelector("#a10a");
+let anwser10aa = document.querySelector("#a10aa");
+let a10aaa = document.querySelector("#aa10aaa");
+let score1 = document.querySelector("h1");
+let points = 0;
+anwser10a.addEventListener("click", function() {
+  let newScore = points + 10;
+  score1.innerHTML = newScore;
+});
+anwser10a.addEventListener("click", function() {
+    let newScore = points + 10;
+    score1.innerHTML = newScore;
+  });
+  anwser10a.addEventListener("click", function() {
+    let newScore = points + 10;
+    score1.innerHTML = newScore;
+  });
