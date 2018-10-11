c66eb80fa23155669fa8ba77256411f23af2ef54
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-11 14:43:51 -0400

    10. Turned divs into buttons

--- knowit.js
+++ knowit.js
@@ -1,3 +1,28 @@
+const points10a= 10;
+const points10b= 10;
+const points10c= 10;
+const points10d= 10;
+const points10e= 10;
+const points20a= 20;
+const points20b= 20;
+const points20c= 20;
+const points20d= 20;
+const points20e= 20;
+const points30a= 30;
+const points30b= 30;
+const points30c= 30;
+const points30d= 30;
+const points30e= 30;
+const points40a= 40;
+const points40b= 40;
+const points40c= 40;
+const points40d= 40;
+const points40e= 40;
+const points50a= 50;
+const points50b= 50;
+const points50c= 50;
+const points50d= 50;
+const points50e= 50;
 /* Get elements
 const modal = document.getElementById('simpleModal');
 //Get open modal button
