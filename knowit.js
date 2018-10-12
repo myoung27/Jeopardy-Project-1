3ef76da4e59075310b29c23bdf2af38413f520e2
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-12 09:43:56 -0400

    13. Added new catagories,ammended functions and added button invisibility

--- knowit.js
+++ knowit.js
@@ -4,33 +4,75 @@ let anwser10aaa = document.querySelector("#a10aaa");
 let anwser10b = document.querySelector("#a10b");
 let anwser10bb = document.querySelector("#a10bb");
 let anwser10bbb = document.querySelector("#a10bbb");
-let anwser10c = document.querySelector("#a10b");
-let anwser10cc = document.querySelector("#a10bb");
-let anwser10ccc = document.querySelector("#a10bbb");
+let anwser10c = document.querySelector("#a10c");
+let anwser10cc = document.querySelector("#a10cc");
+let anwser10ccc = document.querySelector("#a10ccc");
 let score1 = document.querySelector("h1");
-let points = 0;
+var points = 0;
+
 anwser10a.addEventListener("click", function() {
-    points -= 10
-    score1.innerHTML = points;
+  points -= 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[0].style.visibility = "hidden";
 });
 anwser10aa.addEventListener("click", function() {
-    points += 10
-    score1.innerHTML = points;
-  });
-  anwser10aaa.addEventListener("click", function() {
-    points -= 10
-    score1.innerHTML = points;
-  });
+  points += 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[0].style.visibility = "hidden";
+});
+anwser10aaa.addEventListener("click", function() {
+  points -= 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[0].style.visibility = "hidden";
+});
 
-  anwser10b.addEventListener("click", function() {
-    points += 10
+anwser10b.addEventListener("click", function() {
+  points += 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[1].style.visibility = "hidden";
+});
+anwser10bb.addEventListener("click", function() {
+  points -= 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[1].style.visibility = "hidden";
+});
+anwser10bbb.addEventListener("click", function() {
+  points -= 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[1].style.visibility = "hidden";
+});
+anwser10c.addEventListener("click", function() {
+  points += 10;
+  console.log(points);
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[2].style.visibility = "hidden";
+});
+anwser10cc.addEventListener("click", function() {
+  points -= 10;
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[2].style.visibility = "hidden";
+});
+anwser10ccc.addEventListener("click", function() {
+  points -= 10;
+  score1.innerHTML = points;
+  document.getElementsByClassName("btn")[2].style.visibility = "hidden";
+});
+anwser10d.addEventListener("click", function() {
+    points += 10;
+    console.log(points);
     score1.innerHTML = points;
   });
-  anwser10bb.addEventListener("click", function() {
-    points += 10
+  anwser10dd.addEventListener("click", function() {
+    points -= 10;
     score1.innerHTML = points;
-    });
-    anwser10bbb.addEventListener("click", function() {``
-      let newScore = points - 10;
-      score1.innerHTML = newScore;
-    });
+  });
+  anwser10ddd.addEventListener("click", function() {
+    points -= 10;
+    score1.innerHTML = points;
+  });
